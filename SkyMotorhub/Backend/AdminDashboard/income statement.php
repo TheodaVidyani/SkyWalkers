0a3a8f89a88d36af8ab1879c91f9853c42e06897
SadeepSachintha <sadeepsachintha98@gmail.com>
2022-07-20 13:42:01 +0530

    modified

--- SkyMotorhub/Backend/AdminDashboard/income statement.php
+++ SkyMotorhub/Backend/AdminDashboard/income statement.php
@@ -197,20 +197,21 @@
             <!-- Other Elements End -->
 
 
-            <!-- Footer Start -->
-            <div class="container-fluid pt-4 px-4">
-                <div class="bg-light rounded-top p-4">
-                    <div class="row">
-                        <div class="col-12 col-sm-6 text-center text-sm-start">
-                            &copy; <a href="#">Your Site Name</a>, All Right Reserved. 
-                        </div>
-                        <div class="col-12 col-sm-6 text-center text-sm-end">
-                             Designed By <a href="#">HTML Codex</a>
-                        </div>
+           <!-- Footer Start -->
+        <div class="container-fluid pt-4 px-4">
+            <div class="bg-light rounded-top p-4">
+                <div class="row">
+                    <div class="col-12 col-sm-6 text-center text-sm-start">
+                        &copy; <a href="#">SkyMotorhub.lk</a>, All Right Reserved.
+                    </div>
+                    <div class="col-12 col-sm-6 text-center text-sm-end">
+                        Designed By <a href="#">Team SkyWalkers</a>
+                        </br>
                     </div>
                 </div>
             </div>
-            <!-- Footer End -->
+        </div>
+        <!-- Footer End -->
         </div>
         <!-- Content End -->
 
